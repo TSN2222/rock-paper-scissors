1106eb1598f328db14b2ed26793abe21b5ba3b08
TSN2222 <98364618+TSN2222@users.noreply.github.com>
2024-11-27 23:06:18 -0500

    Update syntax and messages | Add global point and round counter variables.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
-    let computerChoice = null;
+    let computerChoice = undefined;
 
     if (random == 1) {
         computerChoice = 'rock';
@@ -12,16 +16,16 @@ function getComputerChoice() {
         computerChoice = 'scissors';
     }
 
-    return computerChoice
+    return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt('Enter Weapon Of Choice:', 'rock, paper, or scissors');
+    let humanChoice = prompt('Enter Weapon Of Choice:');
     humanChoice = humanChoice.toLowerCase();
 
     while ((humanChoice != 'rock') && (humanChoice != 'paper') && (humanChoice != 'scissors')) {
         console.log(`${humanChoice} is not rock, paper, or scissors. Please enter a valid weapon!`);
-        humanChoice = prompt('Enter Weapon Of Choice:');
+        humanChoice = prompt('Enter A VALID Weapon Of Choice:');
         humanChoice = humanChoice.toLowerCase();
     }
 
