ba9f3a1b2eaa9069d6c1d0b8edb55176eb2ab1af
TSN2222 <98364618+TSN2222@users.noreply.github.com>
2024-11-27 22:41:26 -0500

    Create function to get player answer.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
-
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
     let computerChoice = null;
 
     if (random == 1) {
         computerChoice = 'rock';
-    } else if (random == 2) {
+    } 
+    else if (random == 2) {
         computerChoice = 'paper';
-    } else {
+    }
+    else {
         computerChoice = 'scissors';
     }
 
     return computerChoice
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt('Enter Weapon Of Choice:', 'rock, paper, or scissors');
+    humanChoice = humanChoice.toLowerCase();
+
+    while ((humanChoice != 'rock') && (humanChoice != 'paper') && (humanChoice != 'scissors')) {
+        console.log(`${humanChoice} is not rock, paper, or scissors. Please enter a valid weapon!`);
+        humanChoice = prompt('Enter Weapon Of Choice:');
+        humanChoice = humanChoice.toLowerCase();
+    }
+
+    return humanChoice;
 }
